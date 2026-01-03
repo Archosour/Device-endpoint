40c14e1fed9ed258d406286796033b09fdad5b61
Archosaur <marc.vd.klooster@home.nl>
2026-01-03 13:41:33 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,235 @@
+const express = require('express')
+const local_setting = require("./Local_settings.json");
+const app = express()
+const port = local_setting.Request_port;
+
+const redis = require('redis');
+const client = redis.createClient({
+    url : "redis://" + local_setting.Redis_IP + ":" + local_setting.Redis_port
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+client.connect()
+  .then(() => console.log('Connected to Redis'))
+  .catch(err => console.log('Failed to connect to Redis', err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    let Smart_data = new Incomming_Data();
+    Smart_data.Timestamp_recieved = Date.now();
+
+    req.on('data', (chunk) => {
+        Smart_data.Data_raw += chunk;
+    });
+
+    req.on('end', async () => {
+        await Process_incomming_message(Smart_data);
+
+        res.send(Smart_data.Data_raw);
+    });
+})
+
+function Set_device_properties(data, element) {
+    let Smart_data = data;
+
+    if (element.Object == 'Device' && element.Resource == 'Get_identifier') {
+        Smart_data.Identifier = element.Value;
+        console.log(Date.now().toLocaleString(), ": ", element.Value, " Objects: ", Smart_data.Data_parsed.Data.length);
+    }
+
+    if (element.Object == 'MCU' && element.Resource == 'Get_uptime') {
+        Smart_data.Device.Current_uptime = element.Value;
+    }
+
+    if (element.Object == 'Modem' && element.Resource == 'Get_IP_address') {
+        Smart_data.Device.IPv4 = element.Value;
+    }
+
+    if (element.Object == 'Modem' && element.Resource == 'Get_mac_address') {
+        Smart_data.Device.MAC = element.Value;
+    }
+
+    if (element.Object == 'Modem' && element.Resource == 'Get_listen_port') {
+        Smart_data.Device.Listen_port = element.Value;
+    }
+
+    if (element.Object == 'Modem' && element.Resource == 'Get_server_address') {
+        Smart_data.Device.Send_server = element.Value;
+    }
+
+    return Smart_data;
+}
+
+async function Process_incomming_message(Smart_data) {
+    //console.log("Smart data incomming: ", Smart_data, "Already objects: ", Object.keys(Smart_data.Data_parsed).length)
+
+    if (Smart_data.Data_raw.startsWith('{') || Object.keys(Smart_data.Data_parsed).length != 0) {
+        try {
+            if (Object.keys(Smart_data.Data_parsed).length == 0) {
+                Smart_data.Data_parsed = JSON.parse(Smart_data.Data_raw);
+            }
+            
+            if (Smart_data.Data_parsed.Protocol == Protocols.IPSO_v1) {
+                Smart_data.Protocol = Protocols.IPSO_v1;
+
+                Smart_data.Data_parsed.Data.forEach(element => {
+                    Smart_data = Set_device_properties(Smart_data, element);
+
+                    if (element.Object == 'Serial' && element.Resource == 'Set_RX_message') {
+                        let Inner_smart_data = new Incomming_Data();
+                        Inner_smart_data.Timestamp_recieved = Date.now();
+                        Inner_smart_data.Data_raw = element.Value;
+                        
+
+                        try {
+                            Inner_smart_data.Data_parsed = JSON.parse(element.Value);
+                            Process_incomming_message(Inner_smart_data);
+                        }
+                        catch {
+                            let Host_data = Inner_smart_data;
+                            Host_data.Identifier = Smart_data.Identifier;
+                            Handle_logging_data(Host_data);
+                        }
+                        
+                        
+                        //console.log(Inner_smart_data.Data_parsed);
+
+                        
+                    }
+                });
+            }
+        }
+        catch (ex) {
+            console.log(ex);
+        }
+
+        await Handle_sensor_data(Smart_data);
+    }
+    else {
+        console.log(Smart_data.Data_raw);
+    }
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+class Redis_device_update {
+    Identifier = undefined;
+    Incomming_data = undefined;
+
+    constructor(Incomming_data = undefined) {
+        this.Incomming_data = Incomming_data;
+    }
+}
+
+async function Handle_sensor_data(Incomming_data = new Incomming_Data()) {
+  const deviceKey   = `Device:${Incomming_data.Identifier}`;
+  const messagesKey = `${deviceKey}:Messages`;
+  const ioKey       = `${deviceKey}:IO_states`;
+  const timerKey    = `${deviceKey}:Timers`;
+  const exists      = await client.exists(deviceKey);
+
+    if (!exists) {
+        await client.hSet(deviceKey, {
+        Protocol:       Protocols.IPSO_v1,
+        Identifier:     Incomming_data.Identifier,
+        Last_activity:  Date.now(),
+        First_activity: Date.now()
+        });
+    } else {
+        if (Incomming_data.Device.IPv4 != undefined) await client.hSet(deviceKey, 'IPv4', Incomming_data.Device.IPv4);
+        if (Incomming_data.Device.MAC)               await client.hSet(deviceKey, 'MAC', Incomming_data.Device.MAC);
+        if (Incomming_data.Device.Listen_port)       await client.hSet(deviceKey, 'Listen_port', Incomming_data.Device.Listen_port);
+        if (Incomming_data.Device.Send_server)       await client.hSet(deviceKey, 'Send_server', Incomming_data.Device.Send_server);
+        if (Incomming_data.Device.Current_uptime)    await client.hSet(deviceKey, 'Uptime', Incomming_data.Device.Current_uptime);
+                                                     await client.hSet(deviceKey, 'Last_activity', Date.now());
+                                                     await client.hSet(deviceKey, 'Last_activity_hr', new Date().toISOString());
+
+        if (Incomming_data.Data_parsed.Data != undefined) {
+            Incomming_data.Data_parsed.Data.forEach(async element => {
+                if (element.Object == "Digital" && element.Resource == "Get_io_enabled") {
+                    await client.hSet(ioKey, "Digital_" + element.Instance, element.Value);
+                }
+
+                if (element.Object == "Analog" && element.Resource == "Get_ADC") {
+                    await client.hSet(ioKey, "Analog_" + element.Instance, element.Value);
+                }
+
+                if (element.Object == "Timer") {
+                    if (element.Resource == "Get_io_update")      await client.hSet(timerKey, "IO_update_" + element.Instance, element.Value);
+                    if (element.Resource == "Get_message_update") await client.hSet(timerKey, "Message_update_" + element.Instance, element.Value);
+                }
+            })
+        }
+    }
+
+  await client.rPush(messagesKey, Incomming_data.Data_raw);
+}
+
+async function Handle_logging_data(Incomming_data = new Incomming_Data()) {
+    const deviceKey = `Device:${Incomming_data.Identifier}`;
+    const loggingKey = `${deviceKey}:Device_logging`;
+    const exists = await client.exists(deviceKey);
+
+    if (!exists) {
+        await client.hSet(deviceKey, {
+            Protocol: Protocols.IPSO_v1,
+            Identifier: Incomming_data.Identifier,
+            Last_activity: Date.now(),
+            First_activity: Date.now()
+        });
+    } else {
+        await client.hSet(deviceKey, 'Last_activity', Date.now());
+        await client.hSet(deviceKey, 'Last_activity_hr', new Date().toISOString());
+    }
+
+    await client.rPush(loggingKey, Incomming_data.Data_raw);
+}
+
+class Incomming_Data {
+    Identifier = undefined;
+    Data_raw = "";
+    Protocol = Protocols.Unknown;
+    Data_parsed = {}
+    Timestamp_recieved = undefined;
+
+    Raw_database = new Raw_Database();
+
+    Device = new Device();
+}
+
+class Raw_Database {
+    Is_saved = false;
+    Must_be_saved = false;
+    Row_number = undefined;
+}
+
+const Protocols = {
+    Unknown: "Unknown",
+    IPSO_v1: "IPSO_v1",
+}
+
+class Device {
+    Identifier = undefined;
+    MAC = undefined;
+    IPv4 = undefined;
+    Listen_port = undefined;
+    Send_server = undefined;
+    Data_protocol = Protocols.Unknown;
+    Current_uptime = undefined;
+    Stored_data = [];
+
+    Currently_online = false;
+    Last_activity = undefined;
+    First_activity = undefined;
+    Retiered = false;
+
+    constructor(Identifier = undefined) {
+        this.Identifier = Identifier;
+    }
+}
